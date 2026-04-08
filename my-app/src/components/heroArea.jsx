25fa16689a9af69b655b07925998c151c724c0cb
quantumshamim <msamim7617@gmail.com>
2026-04-08 22:12:52 +0600

    React Style

--- my-app/src/components/heroArea.jsx
+++ my-app/src/components/heroArea.jsx
@@ -1,10 +1,23 @@
-import "./hero.css"
+import "./hero.css" //normal
+import Styles from "./heroArea.module.css"
+
 function Hero(){
+    const MyStyle = {
+        color: "yellow",
+        backgroundColor: "black"
+    }
+
     return(
         <>
             <h1 style = {{backgroundColor: "green"}}>This is hero Section</h1>
 
             <h2 id="excss">This section is for External Design</h2>
+
+            <h3 style={MyStyle} >This block is from object for styling</h3>
+
+            <div className={Styles.HeroArea}>
+                <p>styling from module</p>
+            </div>
         </>
     )
 }
