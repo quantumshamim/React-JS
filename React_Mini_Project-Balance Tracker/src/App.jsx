9620f0d0b165a69f68abbeb8e6cd7dfc842fcf9e
quantumshamim <msamim7617@gmail.com>
2026-05-21 10:11:22 +0600

    Mini project-balance tracker

--- React_Mini_Project-Balance Tracker/src/App.jsx
+++ React_Mini_Project-Balance Tracker/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './Components/Header'
+import Balance from './Components/Balance'
 function App() {
 
   return (
     <>
       <Header/>
+      <Balance/>
     </>
   )
 }
--- React_Mini_Project-Balance Tracker/src/Components/Balance.jsx
+++ React_Mini_Project-Balance Tracker/src/Components/Balance.jsx
@@ -1,7 +1,11 @@
 function Balance(){
     return(
         <>
-            
+            <div className="text-center">
+                <h2 className="text-lg">Your Balance</h2>
+                <h3 className="font-bold text-2xl">1000</h3>
+            </div>
         </>
     )
 }
+export default Balance
